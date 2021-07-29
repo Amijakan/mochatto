88c9b5058a889cfd409ae6c60032be927d2189cf
Kon Aoki <kon.aoki.us@gmail.com>
2021-07-28 17:49:49 -0700

    added socket io client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import socketIOClient from 'socket.io-client';
 import logo from './logo.svg';
 import './App.css';
 import request from 'superagent'
 
+const ENDPOINT = 'http://localhost:4000';
+
 function App() {
+	const [response, setResponse] = useState("");
+
+	useEffect(() => {
+		const socket = socketIOClient(ENDPOINT);
+		socket.on("FromAPI", data => {
+			setResponse(data);
+		});
+	}, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+					It's <time dateTime={response}>{response}</time>
         </p>
         <a
           className="App-link"

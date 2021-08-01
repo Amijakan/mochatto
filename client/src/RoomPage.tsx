0702be06134c74bc9708a3c1e76f055a2809e34d
Kon Aoki <kon.aoki.us@gmail.com>
2021-07-31 19:16:39 -0700

    developing queue system for live audio streaming

--- client/src/RoomPage.tsx
+++ client/src/RoomPage.tsx
@@ -6,53 +6,102 @@ const ENDPOINT = 'http://localhost:4000'
 const socket = socketIOClient(ENDPOINT)
 
 function RoomPage() {
-	const [inputOptions, setInputOptions] = useState([{}])
-	const [selectedInput, setSelectedInput] = useState(Device())
 	const [stream, setStream] = useState(new MediaStream())
+	const [blobsQueue, setBlobsQueue] = useState([new Blob()])
+	const [mediaRecorder, setMediaRecorder] = useState(
+		new MediaRecorder(stream)
+	)
 
 	const onSelect = ({ selectedInput, inputOptions, stream }) => {
-		setSelectedInput(selectedInput)
-		setInputOptions(inputOptions)
+		console.log('1. input is selected')
 		setStream(stream)
+		if (stream.active) {
+			setMediaRecorder(new MediaRecorder(stream))
+			setBlobsQueue([new Blob()])
+		}
 	}
 
+	// on render
 	useEffect(() => {
-		let mediaRecorder = new MediaRecorder(stream)
-		if (stream.active) {
-			let chunks
-			mediaRecorder.onstart = function (e) {
-				chunks = []
-			}
-			mediaRecorder.ondataavailable = function (e) {
-				chunks.push(e.data)
-			}
-			mediaRecorder.start()
-			mediaRecorder.onstop = function (e) {
-				var blob = new Blob(chunks, { type: 'audio/ogg; codecs=opus' })
-				socket.emit('SEND_USER_AUDIO', blob)
-				// Start recording again
+		console.log('2. mediarecorder.stream is updated')
+		if (mediaRecorder.state === 'inactive') {
+			if (mediaRecorder.stream.active) {
+				console.log('3. recorder starts')
+				// start recording if it's not already and the stream is active
 				mediaRecorder.start()
 			}
 		}
 
-		//stop recording every second
-		setInterval(function () {
-			console.log(mediaRecorder.state)
-			if (mediaRecorder.state == 'recording') {
-				mediaRecorder.stop()
+		let chunk = [new Blob()]
+
+		mediaRecorder.ondataavailable = (e) => {
+			// push new audio data into the chunks as they become available
+			chunk.push(e.data)
+		}
+
+		mediaRecorder.onstart = (e) => {
+			console.log('4. onstart is called')
+			const audio = document.querySelector('audio')
+			//every second: stop recording and play the next audio chunk in queue
+			setTimeout(() => {
+				if (mediaRecorder.state === 'recording') {
+					console.log('5. stop is called after timer')
+					// stop recording if it is
+					mediaRecorder.stop()
+				}
+				// queue a blob and play it
+				console.log('5.1. blob is queued')
+				console.log(blobsQueue.length)
+				let blob = blobsQueue.shift()
+				console.log(blobsQueue.length)
+				if (audio && blob) {
+					if (blob.size) {
+						console.log(
+							'6. audio source is updated with the new blob'
+						)
+						audio.src = window.URL.createObjectURL(blob)
+					}
+				}
+			}, 2000)
+		}
+
+		// when recorder stops
+		mediaRecorder.onstop = (e) => {
+			console.log('7. onstop is called')
+			// create a blob from the chunk
+			var blobToSend = new Blob(chunk, {
+				type: 'audio/ogg; codecs=opus',
+			})
+
+			// reset the chunk after using it
+			chunk = [new Blob()]
+
+			if (blobToSend.size) {
+				console.log('8. blob is emitted')
+				// emit the audio chunk
+				socket.emit('SEND_USER_AUDIO', blobToSend)
+				//restart the recorder if the stream is active
+				if (mediaRecorder.stream.active) {
+					console.log(
+						'9. recorder is started again and goes back to 4.'
+					)
+					mediaRecorder.start()
+				}
 			}
-		}, 1000)
-		const audio = document.querySelector('audio')
+		}
+
+		// listen for broadcast
 		socket.on('BROADCAST_AUDIO', (broadcast) => {
-			if (audio) {
-				//;(audio as HTMLAudioElement).srcObject = broadcast
-				var blob = new Blob([broadcast], {
-					type: 'audio/ogg; codecs=opus',
-				})
-				audio.src = window.URL.createObjectURL(blob)
-			}
+			console.log(broadcast)
+			let blob = new Blob([broadcast], {
+				type: 'audio/ogg; codecs=opus',
+			})
+			console.log('async. new blob is pushed to queue')
+			blobsQueue.push(blob)
+			console.log(blobsQueue)
+			console.log(blobsQueue.length)
 		})
-	}, [stream])
+	}, [])
 
 	return (
 		<>

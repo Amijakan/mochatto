ad57387d51cb5227dd7f6db22915adeafab4b7b0
Kon Aoki <kon.aoki.us@gmail.com>
2021-07-31 20:13:21 -0700

    removed comments

--- client/src/RoomPage.tsx
+++ client/src/RoomPage.tsx
@@ -1,23 +1,17 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import socketIOClient from 'socket.io-client'
-import { Device, DeviceSelector } from './DeviceSelector'
+import { DeviceSelector } from './DeviceSelector'
 
 const ENDPOINT = 'http://localhost:4000'
 const socket = socketIOClient(ENDPOINT)
 
 function RoomPage() {
-	const [stream, setStream] = useState(new MediaStream())
-
 	const onSelect = ({ selectedInput, inputOptions, stream }) => {
-		console.log('1. input is selected')
-		console.log(stream)
-		console.log(selectedInput)
 		let mediaRecorder = new MediaRecorder(stream)
 		let blobsQueue: Blob[] = []
 
 		if (mediaRecorder.state === 'inactive') {
 			if (mediaRecorder.stream.active) {
-				console.log('2. recorder starts')
 				// start recording if it's not already and the stream is active
 				mediaRecorder.start()
 			}
@@ -31,34 +25,25 @@ function RoomPage() {
 		}
 
 		mediaRecorder.onstart = (e) => {
-			console.log('3. onstart is called')
 			const audio = document.querySelector('audio')
 			//every second: stop recording and play the next audio chunk in queue
 			setTimeout(() => {
 				if (mediaRecorder.state === 'recording') {
-					console.log('4. stop is called after timer')
 					// stop recording if it is
 					mediaRecorder.stop()
 				}
 				// queue a blob and play it
-				console.log('5. blob is queued')
-				console.log(blobsQueue.length)
 				let blob = blobsQueue.shift()
-				console.log(blobsQueue.length)
 				if (audio && blob) {
 					if (blob.size) {
-						console.log(
-							'6. audio source is updated with the new blob'
-						)
 						audio.src = window.URL.createObjectURL(blob)
 					}
 				}
-			}, 500)
+			}, 2000)
 		}
 
 		// when recorder stops
 		mediaRecorder.onstop = (e) => {
-			console.log('7. onstop is called')
 			// create a blob from the chunk
 			var blobToSend = new Blob(chunk, {
 				type: 'audio/ogg; codecs=opus',
@@ -68,29 +53,24 @@ function RoomPage() {
 			chunk = [new Blob()]
 
 			if (blobToSend.size) {
-				console.log('8. blob is emitted')
+				console.log('emit')
 				// emit the audio chunk
 				socket.emit('SEND_USER_AUDIO', blobToSend)
 				//restart the recorder if the stream is active
 				if (mediaRecorder.stream.active) {
-					console.log(
-						'9. recorder is started again and goes back to 4.'
-					)
 					mediaRecorder.start()
 				}
 			}
 		}
 
+		// remove old listener
+		socket.off('BROADCAST_AUDIO')
 		// listen for broadcast
 		socket.on('BROADCAST_AUDIO', (broadcast) => {
-			console.log(broadcast)
 			let blob = new Blob([broadcast], {
 				type: 'audio/ogg; codecs=opus',
 			})
-			console.log('async. new blob is pushed to queue')
 			blobsQueue.push(blob)
-			console.log(blobsQueue)
-			console.log(blobsQueue.length)
 		})
 	}
 

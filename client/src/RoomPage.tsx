7db5f2eabdd686a4ee52500acb9fcd2bb4ab17d1
Kon Aoki <kon.aoki.us@gmail.com>
2021-07-31 21:22:19 -0700

    fixed duplicate event listener bug (the previous commit had a different issue)

--- client/src/RoomPage.tsx
+++ client/src/RoomPage.tsx
@@ -1,14 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import socketIOClient from 'socket.io-client'
-import { DeviceSelector } from './DeviceSelector'
+import { Device, DeviceSelector } from './DeviceSelector'
 
 const ENDPOINT = 'http://localhost:4000'
 const socket = socketIOClient(ENDPOINT)
 
 function RoomPage() {
-	const [listenOnce, setListenOnce] = useState(false)
+	const [prevMediaRecorder, setPrevMediaRecorder] = useState(
+		new MediaRecorder(new MediaStream())
+	)
+
 	const onSelect = ({ selectedInput, inputOptions, stream }) => {
+		prevMediaRecorder.ondataavailable = null
+		prevMediaRecorder.onstart = null
+		prevMediaRecorder.onstop = null
+
 		let mediaRecorder = new MediaRecorder(stream)
+		setPrevMediaRecorder(mediaRecorder)
+
 		let blobsQueue: Blob[] = []
 
 		if (mediaRecorder.state === 'inactive') {
@@ -20,60 +29,58 @@ function RoomPage() {
 
 		let chunk = [new Blob()]
 
-		if (!listenOnce) {
-			setListenOnce(true)
-			mediaRecorder.ondataavailable = (e) => {
-				// push new audio data into the chunks as they become available
-				chunk.push(e.data)
-			}
+		mediaRecorder.ondataavailable = (e) => {
+			// push new audio data into the chunks as they become available
+			chunk.push(e.data)
+		}
 
-			mediaRecorder.onstart = (e) => {
-				const audio = document.querySelector('audio')
-				//every second: stop recording and play the next audio chunk in queue
-				setTimeout(() => {
-					if (mediaRecorder.state === 'recording') {
-						// stop recording if it is
-						mediaRecorder.stop()
-					}
-					// queue a blob and play it
-					let blob = blobsQueue.shift()
-					if (audio && blob) {
-						if (blob.size) {
-							audio.src = window.URL.createObjectURL(blob)
-						}
-					}
-				}, 2000)
-			}
-
-			// when recorder stops
-			mediaRecorder.onstop = (e) => {
-				// create a blob from the chunk
-				var blobToSend = new Blob(chunk, {
-					type: 'audio/ogg; codecs=opus',
-				})
-
-				// reset the chunk after using it
-				chunk = [new Blob()]
-
-				if (blobToSend.size) {
-					console.log('emit')
-					// emit the audio chunk
-					socket.emit('SEND_USER_AUDIO', blobToSend)
-					//restart the recorder if the stream is active
-					if (mediaRecorder.stream.active) {
-						mediaRecorder.start()
+		mediaRecorder.onstart = (e) => {
+			const audio = document.querySelector('audio')
+			//every second: stop recording and play the next audio chunk in queue
+			setTimeout(() => {
+				if (mediaRecorder.state === 'recording') {
+					// stop recording if it is
+					mediaRecorder.stop()
+				}
+				// queue a blob and play it
+				let blob = blobsQueue.shift()
+				if (audio && blob) {
+					if (blob.size) {
+						audio.src = window.URL.createObjectURL(blob)
 					}
 				}
-			}
-
-			// listen for broadcast
-			socket.on('BROADCAST_AUDIO', (broadcast) => {
-				let blob = new Blob([broadcast], {
-					type: 'audio/ogg; codecs=opus',
-				})
-				blobsQueue.push(blob)
-			})
+			}, 1000)
 		}
+
+		// when recorder stops
+		mediaRecorder.onstop = (e) => {
+			// create a blob from the chunk
+			var blobToSend = new Blob(chunk, {
+				type: 'audio/ogg; codecs=opus',
+			})
+
+			// reset the chunk after using it
+			chunk = [new Blob()]
+
+			if (blobToSend.size) {
+				// emit the audio chunk
+				socket.emit('SEND_USER_AUDIO', blobToSend)
+				console.log('emit')
+				//restart the recorder if the stream is active
+				if (mediaRecorder.stream.active) {
+					mediaRecorder.start()
+				}
+			}
+		}
+
+		socket.off('BROADCAST_AUDIO')
+		// listen for broadcast
+		socket.on('BROADCAST_AUDIO', (broadcast) => {
+			let blob = new Blob([broadcast], {
+				type: 'audio/ogg; codecs=opus',
+			})
+			blobsQueue.push(blob)
+		})
 	}
 
 	return (

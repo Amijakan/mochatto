0896f4c6782b21788c6849e2fa3e3dbab7116af4
Kon Aoki <kon.aoki.us@gmail.com>
2021-08-02 14:59:15 -0700

    working live streaming with bugs

--- client/src/RoomPage.tsx
+++ client/src/RoomPage.tsx
@@ -6,16 +6,17 @@ const ENDPOINT = 'http://localhost:4000'
 const socket = socketIOClient(ENDPOINT)
 
 const peerConnection = new RTCPeerConnection({
-	iceServers: [{ urls: 'stun:global.stun.twilio.com:3478?transport=udp' }],
+	iceServers: [{ urls: 'stun:iphone-stun.strato-iphone.de:3478' }],
 })
 
 function RoomPage() {
+	const remoteStream = new MediaStream()
+	const [localStream, setLocalStream] = useState(new MediaStream())
+
 	// when new input is selected
 	const onSelect = ({ selectedInput, inputOptions, stream }) => {
-		stream.getTracks().forEach((track: MediaStreamTrack) => {
-			peerConnection.addTrack(track)
-		})
-
+		setLocalStream(stream)
+		peerConnection.addTrack(stream.getAudioTracks()[0])
 		// send an RTC offer to server to be broadcasted
 		peerConnection
 			.createOffer()
@@ -23,56 +24,78 @@ function RoomPage() {
 				return peerConnection.setLocalDescription(offer)
 			})
 			.then(() => {
-				const data = JSON.stringify({
+				const data = {
 					sdp: peerConnection.localDescription,
 					id: socket.id,
-				})
-				socket.emit('OFFER_OUT', data)
+					type: 'offer',
+				}
+				console.log(data)
+				socket.emit('OFFER_OUT', JSON.stringify(data))
 			})
 			.catch((e) => {
 				console.warn(e)
 			})
-
-		stream.getTracks().forEach((track: MediaStreamTrack) => {})
 	}
 
-	// answer when offer is received
-	socket.on('OFFER_IN', (data) => {
-		const sdp = JSON.parse(data).sdp
-		peerConnection
-			.setRemoteDescription(new RTCSessionDescription(sdp))
-			.then(() => {
-				peerConnection
-					.createAnswer()
-					.then((answer) => {
-						return peerConnection.setLocalDescription(answer)
-					})
-					.then(() => {
-						const data = JSON.stringify({
-							sdp: peerConnection.localDescription,
-							id: socket.id,
-						})
-						socket.emit('ANSWER_OUT', data)
-					})
-					.catch((e) => {
-						console.warn(e)
-					})
-			})
-			.catch((e) => {
-				console.warn(e)
-			})
-	})
+	useEffect(() => {
+		const remotePlayer = document.querySelector('audio')
+		socket.on('connect', () => {
+			console.log(socket.id)
+		})
 
-	// set remote description once answer is recieved
-	socket.on('ANSWER_IN', (data) => {
-		const sdp = JSON.parse(data).id
-		peerConnection.setRemoteDescription(sdp)
-	})
+		peerConnection.ontrack = (event) => {
+			console.log('remote track was added')
+			remoteStream.addTrack(event.track)
+			if (remotePlayer) {
+				remotePlayer.srcObject = remoteStream
+			}
+		}
+
+		// answer when offer is received
+		socket.on('OFFER_IN', (dataString) => {
+			console.log('offer in')
+			const sdp = JSON.parse(dataString).sdp
+			const target = JSON.parse(dataString).id
+			peerConnection
+				.setRemoteDescription(new RTCSessionDescription(sdp))
+				.then(() => {
+					peerConnection
+						.createAnswer()
+						.then((answer) => {
+							console.log(answer)
+							return peerConnection.setLocalDescription(answer)
+						})
+						.then(() => {
+							const data = {
+								sdp: peerConnection.localDescription,
+								id: socket.id,
+								target: target,
+								type: 'answer',
+							}
+							console.log(data)
+							socket.emit('ANSWER_OUT', JSON.stringify(data))
+						})
+						.catch((e) => {
+							console.warn(e)
+						})
+				})
+				.catch((e) => {
+					console.warn(e)
+				})
+		})
+
+		// set remote description once answer is recieved
+		socket.on('ANSWER_IN', (dataString) => {
+			console.log('answer in')
+			const sdp = JSON.parse(dataString).sdp
+			peerConnection.setRemoteDescription(sdp)
+		})
+	}, [])
 
 	return (
 		<>
 			<div>Room page</div>
-			<audio autoPlay></audio>
+			<audio controls autoPlay></audio>
 			<div>Input selector</div>
 			<DeviceSelector onSelect={onSelect} />
 		</>

dfb25996251eb6b042442b649623da82df507818
Kon Aoki <kon.aoki.us@gmail.com>
2021-08-02 16:29:55 -0700

    adding functions to send user info to server

--- client/src/JoinPage.tsx
+++ client/src/JoinPage.tsx
@@ -1,9 +1,15 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { DeviceSelector } from './DeviceSelector'
+import socketIOClient from 'socket.io-client'
+
+const ENDPOINT = 'http://localhost:4000'
+const socket = socketIOClient(ENDPOINT)
 
 function JoinPage() {
-	const audio = document.querySelector('audio')
+	const [audio, setAudio] = useState(document.querySelector('audio'))
+	const [name, setName] = useState('')
+
 	const onSelect = ({ selectedInput, inputOptions, stream }) => {
 		if (audio) {
 			//set the audio src to the stream
@@ -11,11 +17,37 @@ function JoinPage() {
 		}
 	}
 
+	useEffect(() => {
+		setAudio(document.querySelector('audio'))
+	}, [])
+
+	const onJoin = () => {
+		setAudio(null)
+		socket.emit('NEW_USER', name)
+	}
+
 	return (
 		<>
 			<audio autoPlay></audio>
+			Select your input device:
 			<DeviceSelector onSelect={onSelect} />
-			<Link to="/RoomPage">Join</Link>
+			<div>
+				<label>
+					Name:
+					<input
+						type="text"
+						name="name"
+						onChange={(e) => {
+							setName(e.target.value)
+						}}
+					/>
+				</label>
+			</div>
+			<div>
+				<Link to="/RoomPage" onClick={onJoin}>
+					Join
+				</Link>
+			</div>
 		</>
 	)
 }

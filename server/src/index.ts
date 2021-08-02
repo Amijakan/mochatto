4b9d73ae099bbbbd4d7023abdff33ebdb9abe701
Kon Aoki <kon.aoki.us@gmail.com>
2021-08-02 15:00:12 -0700

    working signaling service to facilitate webrtc connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -21,11 +21,24 @@ app.get('/', (req, res) => {
 
 const socketIds: string[] = []
 io.on('connection', (socket) => {
+	console.log('new client: ' + socket.id)
 	if (!socketIds.includes(socket.id)) {
 		socketIds.push(socket.id)
 	}
-	socket.on('OFFER_OUT', (sdp) => {
-		socket.broadcast.emit('OFFER_IN', sdp)
+	socket.on('OFFER_OUT', (dataString) => {
+		console.log('offer out from: ' + JSON.parse(dataString).id)
+		socket.broadcast.emit('OFFER_IN', dataString)
+	})
+	socket.on('ANSWER_OUT', (dataString) => {
+		const target = JSON.parse(dataString).target
+		console.log(
+			'answer out to: ' + target + ' from: ' + JSON.parse(dataString).id
+		)
+		socket.broadcast.to(target).emit('ANSWER_IN', dataString)
+	})
+
+	socket.on('UPDATE_OUT', () => {
+		socket.broadcast.emit('UPDATE_IN')
 	})
 	socket.on('disconnect', () => {
 		console.log('client disconnect')

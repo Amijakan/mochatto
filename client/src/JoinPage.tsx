ce87fef0e7da6f23a434dda601a4bab0a8a6ec5a
Njima1572 <kochi.nakajima@gmail.com>
2021-08-03 19:38:47 -0400

    Formatter

--- client/src/JoinPage.tsx
+++ client/src/JoinPage.tsx
@@ -1,38 +1,37 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { DeviceSelector } from './DeviceSelector'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function JoinPage(props) {
-	const [name, setName] = useState('')
+  const [name, setName] = useState("");
 
-	const onJoin = () => {
-		if (props.socket) {
-			// notify server on join
-			props.socket.emit('NEW_USER', name)
-		}
-	}
+  const onJoin = () => {
+    if (props.socket) {
+      // notify server on join
+      props.socket.emit("NEW_USER", name);
+    }
+  };
 
-	return (
-		<>
-			<div>
-				<label>
-					Name:
-					<input
-						type="text"
-						name="name"
-						onChange={(e) => {
-							setName(e.target.value)
-						}}
-					/>
-				</label>
-			</div>
-			<div>
-				<Link to="/RoomPage" onClick={onJoin}>
-					Join
-				</Link>
-			</div>
-		</>
-	)
+  return (
+    <>
+      <div>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+        </label>
+      </div>
+      <div>
+        <Link to="/RoomPage" onClick={onJoin}>
+          Join
+        </Link>
+      </div>
+    </>
+  );
 }
 
-export default JoinPage
+export default JoinPage;

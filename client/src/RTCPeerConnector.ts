c9ae76dc66af47fa9e1cf317948328ec8056d3e4
Njima1572 <kochi.nakajima@gmail.com>
2021-08-04 15:51:10 -0400

    Using remote RTCPeerConnector

--- client/src/RTCPeerConnector.ts
+++ client/src/RTCPeerConnector.ts
@@ -5,96 +5,99 @@ let socket = null as any;
 
 // initialize socketio (exported)
 const setSocket = (s: any) => {
-  socket = s;
+	socket = s;
 };
 
 const users: User[] = [];
 
 // add user to the network (exported)
 const addUser = (id: string) => {
-  users.push(new User(id));
+	users.push(new User(id));
 };
 
 const getUsers = () => {
-  return users;
+	return users;
 };
 
 const findUserById = (id: string) => {
-  return getUsers().find((user) => {
-    user.id = id;
-  });
+	return getUsers().find((user) => {
+		user.id = id;
+	});
 };
 
 // update tracks for all peer connections (exported)
 const updateAllTracks = (track: MediaStreamTrack) => {
-  users.forEach((user) => {
-    user.updateTrack(track);
-  });
+	users.forEach((user) => {
+		user.updateTrack(track);
+	});
 };
 
 // send out offer to every user on network (exported)
 const sendOffer = () => {
-  // for each user
-  users.forEach((user) => {
-    // emit an offer to the server to be broadcasted
-    user.peerConnection
-      .createOffer()
-      .then((offer) => {
-        return user.peerConnection.setLocalDescription(offer);
-      })
-      .then(() => {
-        const data = {
-          sdp: user.peerConnection.localDescription,
-          id: socket.id,
-          type: "offer",
-        };
-        socket.emit("OFFER", JSON.stringify(data));
-      })
-      .catch((e) => {
-        console.warn(e);
-      });
-  });
+	// for each user
+	users.forEach((user) => {
+		// emit an offer to the server to be broadcasted
+		user.peerConnection
+			.createOffer()
+			.then((offer) => {
+				return user.peerConnection.setLocalDescription(offer);
+			})
+			.then(() => {
+				const data = {
+					sdp: user.peerConnection.localDescription,
+					senderId: socket.id,
+					type: "offer",
+				};
+				socket.emit("OFFER", JSON.stringify(data));
+			})
+			.catch((e) => {
+				console.warn(e);
+			});
+	});
 };
 
 // emit an answer when offer is received
 socket.on("OFFER", (dataString) => {
-  const sdp = JSON.parse(dataString).sdp;
-  const target = JSON.parse(dataString).id;
-  const user = findUserById(target);
-  const peerConnection = (user as User).peerConnection;
-  peerConnection
-    .setRemoteDescription(new RTCSessionDescription(sdp)) // establish connection with the sender
-    .then(() => {
-      peerConnection
-        .createAnswer()
-        .then((answer) => {
-          return peerConnection.setLocalDescription(answer);
-        })
-        .then(() => {
-          const data = {
-            sdp: peerConnection.localDescription,
-            id: socket.id,
-            target: target,
-            type: "answer",
-          };
-          socket.emit("ANSWER", JSON.stringify(data));
-        })
-        .catch((e) => {
-          console.warn(e);
-        });
-    })
-    .catch((e) => {
-      console.warn(e);
-    });
+	const sdp = JSON.parse(dataString).sdp;
+	const target = JSON.parse(dataString).id;
+	const user = findUserById(target);
+	const peerConnection = (user as User).peerConnection;
+	peerConnection
+		.setRemoteDescription(new RTCSessionDescription(sdp)) // establish connection with the sender
+		.then(() => {
+			peerConnection
+				.createAnswer()
+				.then((answer) => {
+					return peerConnection.setLocalDescription(answer);
+				})
+				.then(() => {
+					const data = {
+						sdp: peerConnection.localDescription,
+						senderId: socket.id,
+						receiver: target,
+						type: "answer",
+					};
+					socket.emit("ANSWER", JSON.stringify(data));
+				})
+				.catch((e) => {
+					console.warn(e);
+				});
+		})
+		.catch((e) => {
+			console.warn(e);
+		});
 });
 
 // set remote description once answer is recieved to establish connection
 socket.on("ANSWER", (dataString) => {
-  const sdp = JSON.parse(dataString).sdp;
-  const target = JSON.parse(dataString).id;
-  const user = findUserById(target);
-  const peerConnection = (user as User).peerConnection;
-  peerConnection.setRemoteDescription(sdp);
+	const sdp = JSON.parse(dataString).sdp;
+	const senderId = JSON.parse(dataString).senderId;
+	const receiverId = JSON.parse(dataString).receiverId;
+	const user = findUserById(senderId);
+	if (socket.id === receiverId) {
+		const peerConnection = (user as User).peerConnection;
+		peerConnection.setRemoteDescription(sdp);
+	}
 });
 
 export { addUser, setSocket, updateAllTracks, sendOffer };

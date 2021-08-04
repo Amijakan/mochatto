3a5d868aa8841840883e46912756f704605fe7d8
Kon Aoki <kon.aoki.us@gmail.com>
2021-08-04 10:18:03 -0700

    added stream and player instances to the User class

--- client/src/User.ts
+++ client/src/User.ts
@@ -2,12 +2,25 @@ class User {
 	peerConnection: any;
 	sender: RTCRtpSender;
 	id: string;
+	stream: MediaStream;
+	player: HTMLAudioElement;
 	constructor(id: string) {
 		this.id = id;
 		this.sender = null as unknown as RTCRtpSender;
 		this.peerConnection = new RTCPeerConnection({
 			iceServers: [{ urls: "stun:iphone-stun.strato-iphone.de:3478" }],
 		});
+		this.stream = new MediaStream();
+		this.player = new Audio();
+
+		// listener for when a peer adds a track
+		this.peerConnection.ontrack = (event) => {
+			this.stream.addTrack(event.track);
+			if (this.player) {
+				// set the new stream as the audio source
+				this.player.srcObject = this.stream;
+			}
+		};
 	}
 
 	setSender(s: RTCRtpSender) {

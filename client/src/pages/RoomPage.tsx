ca0c8de9141069e94c8827d9f2b7f0c2d222243d
siberowl <kon.aoki.us@gmail.com>
2021-08-05 16:07:14 -0700

    calling initialization functions for the webrtc network and removed audio dom element

--- client/src/pages/RoomPage.tsx
+++ client/src/pages/RoomPage.tsx
@@ -8,17 +8,33 @@ function RoomPage({ name }) {
 	const { socket } = useContext(SocketContext);
 
 	// when new input is selected
-	const onSelect = ({ selectedInput, inputOptions, stream }) => {};
+	const onSelect = ({ selectedInput, inputOptions, stream }) => {
+		updateAllTracks(stream.getAudioTracks()[0]);
+		sendOffer();
+	};
 
-	socket.on("NEW_USER", ({ name, id }) => {
-		setAnnouncement(name + " has joined.");
-		addUser(id);
-	});
+	useEffect(() => {
+		setSocket(socket);
+
+		// notify server on join
+		socket.emit("NEW_USER", { id: socket.id, name });
+		socket.emit("REQUEST_USERS");
+
+		socket.on("NEW_USER", ({ name, id }) => {
+			setAnnouncement(name + " has joined.");
+			addUser(id);
+		});
+
+		socket.on("REQUEST_USERS", (users) => {
+			users.forEach((user) => {
+				addUser(user.id);
+			});
+		});
+	}, []);
 
 	return (
 		<>
 			<div>Room page</div>
-			<audio autoPlay></audio>
 			<div>Input selector</div>
 			<DeviceSelector onSelect={onSelect} />
 			<div>{announcement}</div>

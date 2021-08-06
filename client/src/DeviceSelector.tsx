bb5476e70a8b3d10c9b7a4e5657d4c6baca000ce
Kon Aoki <kon.aoki.us@gmail.com>
2021-08-06 10:33:57 -0700

    rolled back to older version

--- client/src/DeviceSelector.tsx
+++ client/src/DeviceSelector.tsx
@@ -1,63 +1,52 @@
-import React, { useState, useEffect } from "react";
-import Select from "react-select";
+import React, { useState, useEffect } from 'react'
+import Select from 'react-select'
 
 const Device = () => {
-  const value = "";
-  const label = "";
-  return { value, label };
-};
-
-function DeviceSelector(props) {
-  const [inputOptions, setInputOptions] = useState([{}]);
-  const [selectedInput, setSelectedInput] = useState(Device());
-
-  useEffect(() => {
-    navigator.mediaDevices
-      .getUserMedia({
-        audio: true,
-        video: false,
-      })
-      .then((stream) => {
-        //if microphone permission is allowed
-        //enumerate through media devices
-        navigator.mediaDevices.enumerateDevices().then((devices) => {
-          var inputs = [{}];
-          devices.map((device) => {
-            var input = Device();
-            input.value = device.deviceId;
-            input.label = device.label;
-            inputs.push(input);
-            return null;
-          });
-          setInputOptions(inputs);
-        });
-      });
-  }, []);
-
-  //when new input is selected
-  const handleChangeSelectedInput = (selection) => {
-		console.log(selection);
-    navigator.mediaDevices
-      .getUserMedia({
-				audio: {deviceId: selection.value},
-        video: false,
-      })
-      .then((stream) => {
-				console.log(stream);
-        setSelectedInput(selection);
-        props.onSelect({ selection, inputOptions, stream });
-      });
-  };
-
-  return (
-    <Select
-      value={selectedInput}
-      options={inputOptions}
-      onChange={(selection) => {
-        handleChangeSelectedInput(selection);
-      }}
-    />
-  );
+	const value = ''
+	const label = ''
+	return { value, label }
 }
 
-export { Device, DeviceSelector };
+function DeviceSelector(props) {
+	const [inputOptions, setInputOptions] = useState([{}])
+	const [selectedInput, setSelectedInput] = useState(Device())
+
+	//when new input is selected
+	useEffect(() => {
+		navigator.mediaDevices
+			.getUserMedia({
+				audio: { deviceId: selectedInput.value },
+				video: false,
+			})
+			.then((stream) => {
+				//if microphone permission is allowed
+				//enumerate through media devices
+				navigator.mediaDevices.enumerateDevices().then((devices) => {
+					var inputs = [{}]
+					devices.map((device) => {
+						var input = Device()
+						input.value = device.deviceId
+						input.label = device.label
+						inputs.push(input)
+						return null
+					})
+					setInputOptions(inputs)
+				})
+				props.onSelect({ selectedInput, inputOptions, stream })
+			})
+	}, [selectedInput]) // eslint-disable-line react-hooks/exhaustive-deps
+
+	return (
+		<>
+			<Select
+				value={selectedInput}
+				options={inputOptions}
+				onChange={(selection) => {
+					setSelectedInput(selection)
+				}}
+			/>
+		</>
+	)
+}
+
+export { Device, DeviceSelector }

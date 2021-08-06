920b9e1ce3a79bc0969f850ea1406ba8eec4d0e2
Kon Aoki <kon.aoki.us@gmail.com>
2021-08-06 10:24:17 -0700

    added removeTrack and play() to audio object

--- client/src/User.ts
+++ client/src/User.ts
@@ -1,38 +1,45 @@
 class User {
-  peerConnection: any;
-  sender: RTCRtpSender;
-  id: string;
-  stream: MediaStream;
-  player: HTMLAudioElement;
-  constructor(id: string) {
-    this.id = id;
-    this.sender = null as unknown as RTCRtpSender;
-    this.peerConnection = new RTCPeerConnection({
-      iceServers: [{ urls: "stun:iphone-stun.strato-iphone.de:3478" }],
-    });
-    this.stream = new MediaStream();
-    this.player = new Audio();
+	peerConnection: any;
+	sender: RTCRtpSender;
+	id: string;
+	stream: MediaStream;
+	player: HTMLAudioElement;
+	constructor(id: string) {
+		this.id = id;
+		this.sender = null as unknown as RTCRtpSender;
+		this.peerConnection = new RTCPeerConnection({
+			iceServers: [{ urls: "stun:iphone-stun.strato-iphone.de:3478" }],
+		});
+		this.stream = new MediaStream();
+		this.player = new Audio();
 
-    // listener for when a peer adds a track
-    this.peerConnection.ontrack = (event) => {
-      this.stream.addTrack(event.track);
-      if (this.player) {
-        // set the new stream as the audio source
-        this.player.srcObject = this.stream;
-      }
-    };
-  }
+		// listener for when a peer adds a track
+		this.peerConnection.ontrack = (event) => {
+			if (this.stream.getAudioTracks()[0]) {
+				this.stream.removeTrack(this.stream.getAudioTracks()[0]);
+			}
+			console.log(this.stream.getAudioTracks());
+			this.stream.addTrack(event.track);
+			console.log(this.stream.getAudioTracks());
+			// set the new stream as the audio source
+			this.player.srcObject = this.stream;
+			console.log(this.stream);
+			this.player.play();
+			this.player.autoplay = true;
+		};
+	}
 
-  setSender(s: RTCRtpSender) {
-    this.sender = s;
-  }
+	setSender(s: RTCRtpSender) {
+		this.sender = s;
+	}
 
-  updateTrack(track: MediaStreamTrack) {
-    if (this.sender) {
-      this.peerConnection.removeTrack(this.sender);
-    }
-    this.setSender(this.peerConnection.addTrack(track));
-  }
+	updateTrack(track: MediaStreamTrack) {
+		if (this.sender) {
+			this.peerConnection.removeTrack(this.sender);
+		}
+		console.log(track);
+		this.setSender(this.peerConnection.addTrack(track));
+	}
 }
 
 export default User;

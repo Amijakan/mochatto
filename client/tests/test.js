446a98ba642b8b4a93d6a2595caa2ab782c58f3a
Kon Aoki <kon.aoki.us@gmail.com>
2021-08-08 11:42:31 -0700

    enabled linter on file

--- client/tests/test.js
+++ client/tests/test.js
@@ -1,19 +1,11 @@
-/* eslint-disable */
 import socketIOClient from "socket.io-client";
 import { addUser, updateAllTracks, sendOffer } from "../src/RTCPeerConnector";
 import User from "../src/User";
 
 describe('User class tests', () => {
-  it('joins', () => {
-		const name = ''
-		cy.visit('http://localhost:4500/room')
-		cy.get('input').type('user1')
-		cy.get('button').click()
-
-		cy.contains('div', 'user1 has joined.')
-			.should('be.visible')
-			.then(()=> {
-				cy.visit('http://localhost:4500/room')
-			})
+  it('instantiates user', () => {
+		const id = "sampleid";
+		const user = new User(id);
+		
   })
 })

2b9f96311a241383aad1ecfdb5bb7e9782b8f474
Kon Aoki <kon.aoki.us@gmail.com>
2021-08-11 15:44:00 -0700

    added tests for DeviceSelectorHelper

--- client/cypress/integration/test.js
+++ client/cypress/integration/test.js
@@ -1,28 +1,43 @@
-describe("Server tests", () => {
-	const name = `Cy_${Cypress._.random(1000)}`
+describe("Signaling server tests", () => {
 	it("joins", () => {
-		cy.task("connect");
-		cy.task("join", name).then(
-			user => {
+		const name = `Cy_${Cypress._.random(1000)}`;
+		cy.task("connect").then((id) => {
+			cy.task("join", name).then((user) => {
 				expect(name).to.eq(user.name);
-			}
-		);
+				expect(id).to.eq(user.id);
+			});
+		});
 	});
 	it("requests", () => {
 		cy.task("connect");
-		cy.task("requestUsers").then(
-			users => {
-				expect(users).to.have.length(1);
-			}
-		);
+		cy.task("requestUsers").then((users) => {
+			expect(users).to.have.length(1);
+		});
 	});
 	it("clears", () => {
 		cy.task("connect");
 		cy.task("clearUsers");
-		cy.task("requestUsers").then(
-			users => {
-				expect(users).to.have.length(0);
-			}
-		);
+		cy.task("requestUsers").then((users) => {
+			expect(users).to.have.length(0);
+		});
+	});
+	// offer, answer, and disconnect are untested
+});
+
+import { listInputDevices, selectInputDevice } from "../../src/DeviceSelectorHelper.ts";
+describe("Device Selector Helper Tests", () => {
+	const onError = (e) => {
+		throw new Error(e);
+	}
+	it("lists devices", () => {
+		const inputs = listInputDevices(onError);
+	});
+	it("plays audio from mic", () => {
+		const useStream = (stream) => {
+			const audio = new Audio();
+			audio.srcObj = stream;
+			audio.play();
+		};
+		selectInputDevice("default", useStream, onError);
 	});
 });

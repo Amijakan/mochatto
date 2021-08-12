a84bb3b10c30ca50d6b6524bcb5cf8731b5424cd
siberowl <kon.aoki.us@gmail.com>
2021-08-11 23:09:38 -0700

    updated devicehelper to testing branch version

--- client/src/DeviceSelectorHelper.ts
+++ client/src/DeviceSelectorHelper.ts
@@ -4,21 +4,36 @@ export const Device = (): { value: string; label: string } => {
 	return { value, label };
 };
 
-export const listInputDevices = (): { value: string; label: string }[] => {
+const defaultOnError = (e: MediaStreamError): void => {
+	console.warn(e);
+	return;
+};
+export const listInputDevices = (
+	onError: (MediaStreamError) => void = defaultOnError
+): { value: string; label: string }[] => {
 	const inputs: { value: string; label: string }[] = [];
-	navigator.mediaDevices.enumerateDevices().then((devices) => {
-		devices.map((device) => {
-			const input = Device();
-			input.value = device.deviceId;
-			input.label = device.label;
-			inputs.push(input);
-			return null;
+	navigator.mediaDevices
+		.enumerateDevices()
+		.then((devices) => {
+			devices.map((device) => {
+				const input = Device();
+				input.value = device.deviceId;
+				input.label = device.label;
+				inputs.push(input);
+				return null;
+			});
+		})
+		.catch((e) => {
+			onError(e);
 		});
-	});
 	return inputs;
 };
 
-export const selectInputDevice = (id: string, useStream: (MediaStream) => void): void => {
+export const selectInputDevice = (
+	id: string,
+	useStream: (MediaStream) => void,
+	onError: (MediaStreamError) => void = defaultOnError
+): void => {
 	navigator.mediaDevices
 		.getUserMedia({
 			audio: { deviceId: id },
@@ -26,5 +41,8 @@ export const selectInputDevice = (id: string, useStream: (MediaStream) => void):
 		})
 		.then((stream) => {
 			useStream(stream);
+		})
+		.catch((e) => {
+			onError(e);
 		});
 };

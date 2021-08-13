25875b4f553fc2ccffe87e3b0fef60e5a9b635b1
siberowl <kon.aoki.us@gmail.com>
2021-08-12 19:53:24 -0700

    updated avatar canvas to be simpler

--- client/src/AvatarCanvas.tsx
+++ client/src/AvatarCanvas.tsx
@@ -1,85 +1,46 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 function AvatarCanvas(): JSX.Element {
 	const [pos, setPos] = useState([0, 0]);
-	const [offset, setOffset] = useState([0, 0]);
-	const [isDragging, setIsDragging] = useState(false);
-
-	const container = document.querySelector(".avatar-container") ?? document.createElement("div");
-	const avatar = document.querySelector(".avatar") ?? document.createElement("div");
+	let offset;
 
 	const _onMouseDown = (event) => {
-		setOffset([event.clientX - pos[0], event.clientY - pos[1]]);
-		setIsDragging(true);
+		console.log("down");
+		offset = [event.clientX - pos[0], event.clientY - pos[1]];
+		document.addEventListener("mousemove", _onMouseMove, true);
+		document.addEventListener("mouseup", _onMouseUp, true);
+		event.preventDefault();
 	};
 
 	const _onMouseUp = (event) => {
-		setOffset([0, 0]);
-		setIsDragging(false);
+		console.log("up");
+		document.removeEventListener("mousemove", _onMouseMove, true);
+		document.removeEventListener("mouseup", _onMouseUp, true);
 	};
+
 	const _onMouseMove = (event) => {
 		// world coordinate
 		const mousePos = [event.clientX, event.clientY];
 
-		// viewport coordinates
-		const contRect = container.getBoundingClientRect();
-		const limits = [0, 0, contRect.width, contRect.height];
-
-		const avatarRect = avatar.getBoundingClientRect();
-
-		if (isDragging) {
-			if(mousePos[0]-offset[0] >= limits[0]){
-				if(mousePos[0]-offset[0]+avatarRect.width <= limits[2]){
-					if(mousePos[1]-offset[1] >= limits[1]){
-						if(mousePos[1]-offset[1]+avatarRect.height <= limits[3]){
-							setPos([mousePos[0] - offset[0], mousePos[1]-offset[1]]);
-						}
-						else{
-							setPos([pos[0],limits[3]-avatarRect.height]);
-						}
-					}
-					else{
-						setPos([pos[0],limits[1]]);
-					}
-				}
-				else{
-					setPos([limits[2]-avatarRect.width, pos[1]]);
-				}
-			}
-			else{
-				setPos([limits[0], pos[1]]);
-			}
-		}
+		console.log("pos: "+pos);
+		console.log("offset: "+offset);
+		console.log("mousepos: "+mousePos);
+		setPos([mousePos[0] - offset[0], mousePos[1] - offset[1]]);
 	};
 	return (
 		<>
 			<div
-				className="avatar-container"
-				onMouseUp={_onMouseUp}
-				onMouseMove={_onMouseMove}
+				className="avatar"
+				onMouseDown={_onMouseDown}
 				style={{
-					background: "black",
+					width: "50px",
+					height: "50px",
+					background: "red",
 					position: "absolute",
-					left: "10%",
-					width: "80%",
-					height: "80%",
-					padding: "0",
-					margin: "0",
+					left: pos[0],
+					top: pos[1],
 				}}
-			>
-				<div
-					className="avatar"
-					onMouseDown={_onMouseDown}
-					style={{
-						width: "50px",
-						height: "50px",
-						background: "red",
-						position: "absolute",
-						left: pos[0],
-						top: pos[1],
-					}}
-				></div>
-			</div>
+			></div>
 		</>
 	);
 }

e2a1af424942b7f45417cd0a398d35838b93470e
Kon Aoki <kon.aoki.us@gmail.com>
2021-08-15 22:19:37 -0700

    rendering avatarDOMs in return directly

--- client/src/AvatarCanvas.tsx
+++ client/src/AvatarCanvas.tsx
@@ -1,18 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Avatar from "./Avatar";
 import AvatarDOM from "./AvatarDOM";
-import { getUsers, updateAvatarPositions } from "./RTCPeerConnector";
 
-function AvatarCanvas(): JSX.Element {
-	const [avatarDOMs, setAvatarDOMs] = useState<JSX.Element[]>([]);
-	const [avatars, setAvatars] = useState<Avatar[]>([]);
+function AvatarCanvas({ avatars, setAvatars }: { avatars: Avatar[], setAvatars: (any) => void }): JSX.Element {
 	let offset;
-
-	useEffect(() => {
-		avatars.push(new Avatar());
-		const avatarDOM = <AvatarDOM key={0} onMouseDown={_onMouseDown} pos={avatars[0].getPos()} />;
-		setAvatarDOMs([...avatarDOMs, avatarDOM]);
-	}, []);
 
 	const _onMouseDown = (event) => {
 		offset = [event.clientX - avatars[0].getPos()[0], event.clientY - avatars[0].getPos()[1]];
@@ -21,7 +12,7 @@ function AvatarCanvas(): JSX.Element {
 		event.preventDefault();
 	};
 
-	const _onMouseUp = (event) => {
+	const _onMouseUp = () => {
 		document.removeEventListener("mousemove", _onMouseMove, true);
 		document.removeEventListener("mouseup", _onMouseUp, true);
 	};
@@ -29,27 +20,30 @@ function AvatarCanvas(): JSX.Element {
 	const _onMouseMove = (event) => {
 		// world coordinate
 		const mousePos = [event.clientX, event.clientY];
-		avatars[0].setPos([mousePos[0] - offset[0], mousePos[1] - offset[1]]);
-		console.log(avatars[0].getPos());
-		updateAvatarPositions(avatars[0].getPos());
+		const newAvatar = new Avatar();
+		newAvatar.setPos([mousePos[0] - offset[0], mousePos[1] - offset[1]]);
+		setAvatars([newAvatar, ...avatars.slice(1, avatars.length)]);
 	};
 
-	const addAvatar = (user) => {
-		avatars.push(new Avatar());
-		const index = avatars.length;
-		const avatarDOM = (
-			<AvatarDOM
-				key={index}
-				onMouseDown={(e) => {
-					console.log();
-				}}
-				pos={avatars[index].getPos()}
-			/>
-		);
-		setAvatarDOMs([...avatarDOMs, avatarDOM]);
-	};
-
-	return <>{avatarDOMs}</>;
+	return (
+		<>
+			{avatars.map((avatar, index) => {
+				if (index === 0) {
+					return <AvatarDOM key={index} onMouseDown={_onMouseDown} pos={avatars[index].getPos()} />;
+				} else {
+					return (
+						<AvatarDOM
+							key={index}
+							onMouseDown={(e) => {
+								console.log();
+							}}
+							pos={avatars[index].getPos()}
+						/>
+					);
+				}
+			})}
+		</>
+	);
 }
 
 export default AvatarCanvas;

f1d2a27c7b6b9e81232ba01abc771cc626cc0e3e
Kon Aoki <kon.aoki.us@gmail.com>
2021-08-15 22:21:28 -0700

    holding avatars instead of the canvas

--- client/src/pages/RoomPage.tsx
+++ client/src/pages/RoomPage.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import { SocketContext } from "../SocketIOContext";
 import { DeviceSelector } from "../DeviceSelector";
+import Avatar from "../Avatar";
 import AvatarCanvas from "../AvatarCanvas";
 import {
 	addUser,
@@ -10,6 +11,7 @@ import {
 	openOfferListener,
 	openAnswerListener,
 	getUsers,
+	updateAvatarPositions,
 } from "../RTCPeerConnector";
 import {
 	notifyAndRequestNetworkInfo,
@@ -22,6 +24,7 @@ import PropTypes from "prop-types";
 function RoomPage({ name }: { name: string }): JSX.Element {
 	const [announcement, setAnnouncement] = useState("");
 	const { socket } = useContext(SocketContext);
+	const [avatars, setAvatars] = useState<Avatar[]>([new Avatar()]);
 
 	// when new input is selected
 	const onSelect = (stream) => {
@@ -29,14 +32,48 @@ function RoomPage({ name }: { name: string }): JSX.Element {
 		sendOffer(socket);
 	};
 
+	const onJoin = ({name, id}) => {
+		setAnnouncement(name + " has joined.");
+		if(id != socket.id){
+			addUser(id);
+		}
+		// add a new avatar on join
+		const tempAvatars: Avatar[] = [];
+		getUsers().map((user) => {
+			user.onAvatarDCMessageCallback = onAvatarDCMessageCallback;
+			tempAvatars.push(user.avatar);
+		});
+		setAvatars([avatars[0], ...(tempAvatars.slice(1, tempAvatars.length))]);
+	};
+
+	const onAvatarDCMessageCallback = () => {
+		const tempAvatars: Avatar[] = [];
+		getUsers().map((user) => {
+			tempAvatars.push(user.avatar);
+		});
+		setAvatars([avatars[0], ...(tempAvatars.slice(1, tempAvatars.length))]);
+	}
+
+	// wrapper for setAvatar to be passed to AvatarCanvas
+	const _setAvatars = (_avatars) => {
+		setAvatars(_avatars);
+	};
+
 	useEffect(() => {
 		notifyAndRequestNetworkInfo(socket, name);
-		openJoinListener(socket, setAnnouncement, addUser);
+		openJoinListener(socket, onJoin);
 		openLeaveListener(socket, setAnnouncement, removeUser);
 		openRequestUsersListener(socket, addUser);
 		openOfferListener(getUsers(), socket);
 		openAnswerListener(getUsers(), socket);
 	}, []);
+
+	useEffect(() => {
+		if(avatars[0]){
+			updateAvatarPositions(avatars[0].getPos());
+		}
+	}, [avatars[0]]);
+
 
 	return (
 		<>
@@ -44,7 +81,7 @@ function RoomPage({ name }: { name: string }): JSX.Element {
 			<div>Input selector</div>
 			<DeviceSelector onSelect={onSelect} />
 			<div>{announcement}</div>
-			<AvatarCanvas />
+			<AvatarCanvas avatars={avatars} setAvatars={_setAvatars} />
 		</>
 	);
 }

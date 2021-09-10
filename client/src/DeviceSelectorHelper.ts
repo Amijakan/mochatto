1f9928b41c5e6da75017cb9b15c6b9946170344d
Njima1572 <kochi.nakajima@gmail.com>
2021-09-09 23:57:13 -0400

    Formatter

--- client/src/DeviceSelectorHelper.ts
+++ client/src/DeviceSelectorHelper.ts
@@ -1,56 +1,56 @@
 export const Device = (): { value: string; label: string } => {
-	const value = "";
-	const label = "";
-	return { value, label };
+  const value = "";
+  const label = "";
+  return { value, label };
 };
 
 const defaultOnError = (e: MediaStreamError): void => {
-	console.warn(e);
-	return;
+  console.warn(e);
+  return;
 };
 
 export const listInputDevices = (
-	onError: (MediaStreamError) => void = defaultOnError
+  onError: (MediaStreamError) => void = defaultOnError
 ): { value: string; label: string }[] => {
-	const inputs: { value: string; label: string }[] = [];
-	navigator.mediaDevices
-		.enumerateDevices()
-		.then((devices) => {
-			devices.map((device) => {
-				if (device.kind === "audioinput") {
-					const input = Device();
-					input.value = device.deviceId;
-					input.label = device.label;
-					inputs.push(input);
-					return null;
-				}
-			});
-		})
-		.catch((e) => {
-			onError(e);
-		});
-	return inputs;
+  const inputs: { value: string; label: string }[] = [];
+  navigator.mediaDevices
+    .enumerateDevices()
+    .then((devices) => {
+      devices.map((device) => {
+        if (device.kind === "audioinput") {
+          const input = Device();
+          input.value = device.deviceId;
+          input.label = device.label;
+          inputs.push(input);
+          return null;
+        }
+      });
+    })
+    .catch((e) => {
+      onError(e);
+    });
+  return inputs;
 };
 
 export const selectInputDevice = (
-	id: string,
-	useStream: (MediaStream) => void,
-	onError: (MediaStreamError) => void = defaultOnError
+  id: string,
+  useStream: (MediaStream) => void,
+  onError: (MediaStreamError) => void = defaultOnError
 ): void => {
-	navigator.mediaDevices
-		.getUserMedia({
-			audio: {
-				deviceId: id,
-				autoGainControl: false,
-				echoCancellation: false,
-				noiseSuppression: false,
-			},
-			video: false,
-		})
-		.then((stream) => {
-			useStream(stream);
-		})
-		.catch((e) => {
-			onError(e);
-		});
+  navigator.mediaDevices
+    .getUserMedia({
+      audio: {
+        deviceId: id,
+        autoGainControl: false,
+        echoCancellation: false,
+        noiseSuppression: false,
+      },
+      video: false,
+    })
+    .then((stream) => {
+      useStream(stream);
+    })
+    .catch((e) => {
+      onError(e);
+    });
 };

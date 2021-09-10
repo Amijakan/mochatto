d1704a93502711fab8184b6cbfb1eecac3c00c2e
Kon Aoki <kon.aoki.us@gmail.com>
2021-09-09 20:24:33 -0700

    using reducer

--- client/src/contexts/PositionsContext.tsx
+++ client/src/contexts/PositionsContext.tsx
@@ -1,17 +1,38 @@
-import React, { useState, createContext } from "react";
+import React, { createContext, useReducer } from "react";
+
+interface PeerPosition {
+  [key: string]: [number, number];
+}
+
+interface State {
+  peerPositions: PeerPosition[];
+}
+
+interface ActionType {
+  type: string;
+  id: string;
+  position: [number, number];
+}
 
 export const PositionsContext = createContext<any>({});
 
 export const PositionsProvider = ({ children }: { children: any }) => {
-  const [peerPositions, setPeerPositions] = useState<any>({});
-  const addPositions = (userId: number) => (positions: any) => {
-		setPeerPositions({ ...peerPositions, [userId]: positions });
+  const reducer = (state: State, action: ActionType) => {
+    switch (action.type) {
+      case "add":
+        return { ...state.peerPositions, [action.id]: action.position };
+      default:
+        return peerPositions;
+    }
+  };
+  const [peerPositions, dispatch] = useReducer(reducer, {});
+  const addPositions = (userId: string) => (position: [number, number]) => {
+    dispatch({ type: "add", position: position, id: userId });
   };
   return (
     <PositionsContext.Provider
       value={{
         peerPositions: peerPositions,
-        setPeerPositions: setPeerPositions,
         addPositions: addPositions,
       }}
     >
--- client/src/pages/RoomPage.tsx
+++ client/src/pages/RoomPage.tsx
@@ -27,7 +27,7 @@ function RoomPage({ name }: { name: string }): JSX.Element {
   const [announcement, setAnnouncement] = useState("");
   const { socket } = useContext(SocketContext);
   const [selfPosition, setSelfPosition] = useState<[number, number]>([0, 0]);
-	const { peerPositions, setPeerPositions, addPositions } = useContext(PositionsContext);
+  const { peerPositions, addPositions } = useContext(PositionsContext);
 
   // when new input is selected
   const onSelect = (stream) => {
@@ -46,8 +46,7 @@ function RoomPage({ name }: { name: string }): JSX.Element {
   // set the user setPosition callback to change the state
   // add the user
   const setNewUser = (userId) => {
-		const user = new User(userId);
-    console.log(userId);
+    const user = new User(userId);
     user.setPosition = addPositions(userId);
     addUser(user);
   };

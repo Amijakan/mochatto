319066a00a25766b8e9e334e6518af0a4d94c4a4
Kon Aoki <kon.aoki.us@gmail.com>
2021-09-10 23:29:59 -0700

    changed back

--- client/src/contexts/SocketIOContext.tsx
+++ client/src/contexts/SocketIOContext.tsx
@@ -2,16 +2,22 @@ import React, { createContext, useState } from "react";
 import socketIOClient, { Socket } from "socket.io-client";
 import PropTypes from "prop-types";
 
-export const SocketContext = createContext<{ socket: Socket }>({
+export const SocketContext = createContext<{ socket: Socket; peerConnection: RTCPeerConnection }>({
   socket: null as unknown as Socket,
+  peerConnection: null as unknown as RTCPeerConnection,
 });
 
 export const SocketProvider = ({ children }: { children: JSX.Element }): JSX.Element => {
   const ENDPOINT = "http://localhost:4000/";
   const [socket, setSocket] = useState(socketIOClient(ENDPOINT));
+  const [peerConnection, setPeerConnection] = useState(
+    new RTCPeerConnection({
+      iceServers: [{ urls: "stun:iphone-stun.strato-iphone.de:3478" }],
+    })
+  );
 
   return (
-    <SocketContext.Provider value={{ socket }}>{children}</SocketContext.Provider>
+    <SocketContext.Provider value={{ socket, peerConnection }}>{children}</SocketContext.Provider>
   );
 };
 

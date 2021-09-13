385c31ff55ba0e67af2cee60fb30675fac9a9462
Kon Aoki <kon.aoki.us@gmail.com>
2021-09-13 03:32:34 -0700

    added new context for sharing media stream between join and room pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,17 +3,20 @@ import Pages from "./pages";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { SocketProvider } from "./contexts/SocketIOContext";
 import { PositionsProvider } from "./contexts/PositionsContext";
+import { DeviceProvider } from "./contexts/DeviceContext";
 
 const App = (): JSX.Element => {
   return (
     <div>
       <SocketProvider>
         <PositionsProvider>
+          <DeviceProvider>
           <BrowserRouter>
             <Switch>
               <Route path="/:room_id" component={Pages} />
             </Switch>
           </BrowserRouter>
+          </DeviceProvider>
         </PositionsProvider>
       </SocketProvider>
     </div>

e8dffddd8a8f79bc33e6d84a43bfeee1627cc5c0
Kon Aoki <kon.aoki.us@gmail.com>
2021-09-12 22:26:39 -0700

    using new context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,17 +3,21 @@ import Pages from "./pages";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { SocketProvider } from "./contexts/SocketIOContext";
 import { PositionsProvider } from "./contexts/PositionsContext";
+import { UserInfoProvider } from "./contexts/UserInfoContext";
 
 const App = (): JSX.Element => {
   return (
     <div>
       <SocketProvider>
         <PositionsProvider>
+          <UserInfoProvider>
           <BrowserRouter>
             <Switch>
               <Route path="/:room_id" component={Pages} />
             </Switch>
           </BrowserRouter>
+          </UserInfoProvider>
+
         </PositionsProvider>
       </SocketProvider>
     </div>

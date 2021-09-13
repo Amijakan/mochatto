63ff4148459dd25aba08f67a900a4d20680d74fc
Kon Aoki <kon.aoki.us@gmail.com>
2021-09-12 22:27:20 -0700

    changed interface and added reducer for keeping track

--- client/src/contexts/UserInfoContext.tsx
+++ client/src/contexts/UserInfoContext.tsx
@@ -1,15 +1,54 @@
-import React, { createContext, useState  } from "react";
+import React, { createContext, useReducer, useCallback } from "react";
 
-interface UserInfo {
+export interface UserInfo {
   name: string;
-  color: string;
+  avatarColor: {
+    background: string;
+    border: string;
+  };
 }
-export const UserInfoContext = createContext<{ userInfo: UserInfo }>({
-  userInfo: null as unknown as UserInfo,
-});
+
+interface Action {
+  type: string;
+  id: string;
+  userInfo: UserInfo;
+}
+
+export const UserInfoContext = createContext<any>({});
 
 export const UserInfoProvider = ({ children }: { children: JSX.Element }): JSX.Element => {
-  const [userInfo, setUserInfo] = useState(null as unknown as UserInfo);
-
-  return <UserInfoContext.Provider value={{ userInfo }}>{children}</UserInfoContext.Provider>;
+  // reducer for adding a avatar position to the list render
+  const reducer = (userInfos: { [key: string]: UserInfo }, action: Action) => {
+    switch (action.type) {
+      case "add":
+        console.debug(action);
+        return { ...userInfos, [action.id]: action.userInfo };
+      case "remove": {
+        const { [action.id]: _toRemove, ...removed } = userInfos;
+        console.log(_toRemove);
+        return removed;
+      }
+      default:
+        return userInfos;
+    }
+  };
+  const [userInfos, dispatch] = useReducer<React.Reducer<{ [key: string]: UserInfo }, Action>>(
+    reducer,
+    {}
+  );
+  // dispatching the action for adding a position
+  const addUserInfo = useCallback(
+    (userId: string) => (userInfo: UserInfo) => {
+      dispatch({ type: "add", userInfo, id: userId });
+    },
+    []
+  );
+  const removeUserInfo = useCallback((userId: string) => {
+    dispatch({ type: "remove", userInfo: null as unknown as UserInfo, id: userId });
+  }, []);
+  return (
+    <UserInfoContext.Provider value={{ userInfos, addUserInfo, removeUserInfo }}>
+      {children}
+    </UserInfoContext.Provider>
+  );
 };

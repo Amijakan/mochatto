7ff8a5bea9a4fd04a6710768cb5ff66105a6ddb4
Kon Aoki <kon.aoki.us@gmail.com>
2021-09-13 05:04:53 -0700

    added audio visualizer

--- client/src/pages/JoinPage.tsx
+++ client/src/pages/JoinPage.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { SocketContext } from "../contexts/SocketIOContext";
 import { DeviceContext } from "../contexts/DeviceContext";
 import { DeviceSelector } from "../DeviceSelector";
@@ -13,6 +13,8 @@ const JoinPage = ({
 }): JSX.Element => {
   const { socket } = useContext(SocketContext);
   const { stream, setStream } = useContext(DeviceContext);
+  const [gain, setGain] = useState(0);
+  const [drawInterval, setDrawInterval] = useState(0);
   const onJoinClicked = () => {
     if (socket) {
       setJoined(true);
@@ -30,11 +32,45 @@ const JoinPage = ({
     setStream(_stream);
   };
 
+  useEffect(() => {
+    if (stream) {
+      if (stream.active) {
+        const context = new AudioContext();
+        const source = context.createMediaStreamSource(stream);
+        const analyser = context.createAnalyser();
+        analyser.smoothingTimeConstant = 0.3;
+        analyser.fftSize = 1024;
+
+        // chain mic -> analyser -> processor -> context
+        source.connect(analyser); // feed mic audio into analyser
+
+        if (drawInterval) {
+          window.clearInterval(drawInterval);
+          setGain(0);
+        }
+        const draw = () => {
+          const array = new Uint8Array(analyser.fftSize);
+          analyser.getByteFrequencyData(array);
+
+          let sum = 0;
+          array.forEach((e, i) => {
+            sum += e * 4;
+          });
+          const average = sum / array.length;
+          if (average != 0) {
+            setGain(average);
+          }
+        };
+        setDrawInterval(window.setInterval(draw, 50));
+      }
+    }
+  }, [stream]);
+
   return (
     <>
       <div>
         <label>
-          Name:
+          <div>Name:</div>
           <input
             type="text"
             name="name"
@@ -44,7 +80,17 @@ const JoinPage = ({
           />
         </label>
       </div>
-      <DeviceSelector onSelect={onSelect} />
+      <div>
+        <div>Select audio device:</div>
+        <DeviceSelector onSelect={onSelect} />
+        <div
+          style={{
+            width: gain.toString() + "px",
+            height: "10px",
+            background: "black",
+          }}
+        ></div>
+      </div>
       <div>
         <button onClick={() => onJoinClicked()}>Join</button>
       </div>

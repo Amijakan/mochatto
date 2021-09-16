077c4202670e1f66b04e9f5f184b3fa30f2c9ce8
Kon Aoki <kon.aoki.us@gmail.com>
2021-09-16 10:17:35 -0700

    adjusted size

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Div } from "atomize";
 const Logo = ({ height = "20" }: { width?: string; height?: string }): JSX.Element => {
   return (
-    <Div d="flex" p="0.5rem" align="center">
+    <Div d="flex" p="0.7rem" align="center">
       <img
         height={height}
         src="https://github.com/Amijakan/mochatto/blob/main/logo/logo_light.png?raw=true"

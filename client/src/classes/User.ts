a4292defa9f7266cb6b6515d533dd15f2f46e8d8
Kon Aoki <kon.aoki.us@gmail.com>
2021-09-16 11:14:40 -0700

    tried adding servers

--- client/src/classes/User.ts
+++ client/src/classes/User.ts
@@ -23,7 +23,45 @@ class User {
     this.avatarDC = null as unknown as RTCDataChannel;
     this.userInfoDC = null as unknown as RTCDataChannel;
     // initialize with a free public STUN server to find out public ip, NAT type, and internet side port
-    this.peerConnection = new RTCPeerConnection();
+    this.peerConnection = new RTCPeerConnection({
+      iceServers: [
+        { urls: "stun:iphone-stun.strato-iphone.de:3478" },
+        { urls: "stun:stun01.sipphone.com" },
+        { urls: "stun:stun.ekiga.net" },
+        { urls: "stun:stun.fwdnet.net" },
+        { urls: "stun:stun.ideasip.com" },
+        { urls: "stun:stun.iptel.org" },
+        { urls: "stun:stun.rixtelecom.se" },
+        { urls: "stun:stun.schlund.de" },
+        { urls: "stun:stun.l.google.com:19302" },
+        { urls: "stun:stun1.l.google.com:19302" },
+        { urls: "stun:stun2.l.google.com:19302" },
+        { urls: "stun:stun3.l.google.com:19302" },
+        { urls: "stun:stun4.l.google.com:19302" },
+        { urls: "stun:stunserver.org" },
+        { urls: "stun:stun.softjoys.com" },
+        { urls: "stun:stun.voiparound.com" },
+        { urls: "stun:stun.voipbuster.com" },
+        { urls: "stun:stun.voipstunt.com" },
+        { urls: "stun:stun.voxgratia.org" },
+        { urls: "stun:stun.xten.com" },
+        {
+          urls: "turn:numb.viagenie.ca",
+          credential: "muazkh",
+          username: "webrtc@live.com",
+        },
+        {
+          urls: "turn:192.158.29.39:3478?transport=udp",
+          credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+          username: "28224511:1379330808",
+        },
+        {
+          urls: "turn:192.158.29.39:3478?transport=tcp",
+          credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+          username: "28224511:1379330808",
+        },
+      ],
+    });
     this.multiplier = 0;
     this.stream = new MediaStream();
     this.visualizer = null as unknown as AudioVisualizer;

920e7895dbb60460478546160f092a6b130c478b
Kon Aoki <kon.aoki.us@gmail.com>
2021-09-20 07:59:19 -0700

    added font

--- client/src/contexts/index.tsx
+++ client/src/contexts/index.tsx
@@ -3,15 +3,24 @@ import { SocketContext, SocketProvider } from "./SocketIOContext";
 import { DeviceContext, DeviceProvider } from "./DeviceContext";
 import { UserInfoContext, UserInfoProvider } from "./UserInfoContext";
 import { PositionsContext, PositionsProvider } from "./PositionsContext";
+import { ThemeProvider } from "atomize";
+
+const theme = {
+  fontFamily: {
+    primary: "DM Sans, sans-serif",
+  },
+};
 
 const Providers = ({ children }: { children: ReactElement }): JSX.Element => (
-  <SocketProvider>
-    <PositionsProvider>
-      <DeviceProvider>
-        <UserInfoProvider>{children}</UserInfoProvider>
-      </DeviceProvider>
-    </PositionsProvider>
-  </SocketProvider>
+  <ThemeProvider theme={theme}>
+    <SocketProvider>
+      <PositionsProvider>
+        <DeviceProvider>
+          <UserInfoProvider>{children}</UserInfoProvider>
+        </DeviceProvider>
+      </PositionsProvider>
+    </SocketProvider>
+  </ThemeProvider>
 );
 
 export { DeviceContext, PositionsContext, UserInfoContext, SocketContext };

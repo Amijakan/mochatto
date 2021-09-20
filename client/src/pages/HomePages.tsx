f139adc3da7c0e70480f4a670d66f5406313b204
Kon Aoki <kon.aoki.us@gmail.com>
2021-09-20 08:00:38 -0700

    added search and button for joining a room

--- client/src/pages/HomePages.tsx
+++ client/src/pages/HomePages.tsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { BaseTemplate } from "../templates";
 import { Div } from "atomize";
-import { Button, Card, Text } from "../components/atomize_wrapper";
+import { Button, Card, Text, Input } from "../components/atomize_wrapper";
+import { SocketContext } from "../contexts";
 
 const HomePage = (): JSX.Element => {
+  const { socket } = useContext(SocketContext);
+  const [roomExists, setRoomExists] = useState(false);
+  const [roomId, setRoomId] = useState("");
+
+  useEffect(() => {
+    if (socket) {
+      socket.on("NUM_USERS", (usersNum) => {
+        if (usersNum === 0 || usersNum === null) {
+          setRoomExists(false);
+        } else {
+          setRoomExists(true);
+        }
+      });
+    }
+  }, [socket]);
+
+  useEffect(() => {
+    if (socket) {
+      socket.emit("NUM_USERS", "/" + roomId);
+    }
+  }, [roomId]);
+
   return (
     <BaseTemplate>
-      <Div justify="space-around" p="5rem" align="center">
+      <Div flexDir="column" d="flex" justify="space-around" p="5rem" align="center">
         <Text textColor="#212121" textAlign="center" textSize="5rem" textWeight="700">
           <Div>Your open-source</Div>
           <Div>virtual meeting space</Div>
@@ -16,14 +39,24 @@ const HomePage = (): JSX.Element => {
             A peer-to-peer voice chat app focused on simplicity.
           </Text>
         </Div>
+        <Div flexDir="column" p={{ b: "1rem" }} d="flex" justify="center" w="30%">
+          <Input
+            textAlign="center"
+            placeholder="Room ID"
+            type="text"
+            name="name"
+            onChange={(e) => {
+              setRoomId(e.target.value);
+            }}
+          />
+        </Div>
         <Button
-          align="center"
-          w="45%"
+          w="30%"
           onClick={() => {
             console.log("hi");
           }}
         >
-          Join
+          {roomExists ? "Join" : "Create"}
         </Button>
       </Div>
     </BaseTemplate>

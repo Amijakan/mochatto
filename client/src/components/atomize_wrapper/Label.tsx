828b0a738ac53fc743c6d1cc3b809a9f8fd00819
Kon Aoki <kon.aoki.us@gmail.com>
2021-09-21 03:10:47 -0700

    added label to wrapper

---
+++ client/src/components/atomize_wrapper/Label.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { Label } from "atomize";
+
+const _Label = (props: any) => <Label fontFamily="primary" {...props} />; // eslint-disable-line
+export default _Label;

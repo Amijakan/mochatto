6d4e799e1343917ee1e4c783dea50f7f760c28e9
Kon Aoki <kon.aoki.us@gmail.com>
2021-09-23 07:34:31 -0700

    supplying default mediastream

--- client/src/contexts/DeviceContext.tsx
+++ client/src/contexts/DeviceContext.tsx
@@ -5,7 +5,7 @@ import { Device } from "../components/DeviceSelectorHelper";
 export const DeviceContext = createContext<any>({});
 
 export const DeviceProvider = ({ children }: { children: JSX.Element }): JSX.Element => {
-  const [stream, setStream] = useState(null as unknown as MediaStream);
+  const [stream, setStream] = useState(new MediaStream());
   const [inputOptions, setInputOptions] = useState([{}]);
   const [selectedInput, setSelectedInput] = useState(Device());
 

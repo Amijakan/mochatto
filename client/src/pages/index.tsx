4c14b9106c76aedb0fcb425dfe9a40ce629774dd
Njima1572 <kochi.nakajima@gmail.com>
2021-09-25 10:30:19 -0400

    Removed console.log

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -5,8 +5,8 @@ import RoomPage from "./RoomPage";
 const Pages = (): JSX.Element => {
   const [name, setName] = useState<string>(sessionStorage.getItem("username") || "");
   const [joined, setJoined] = useState<boolean>(false);
+
   useEffect(() => {
-    console.log(sessionStorage.getItem("username"));
     if (joined && name) {
       sessionStorage.setItem("username", name);
     }

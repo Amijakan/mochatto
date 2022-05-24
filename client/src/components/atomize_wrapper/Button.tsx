460c3739e85c4ceafb0e0d0dc9f54fba0c99d6b2
Njima1572 <kochi.nakajima@gmail.com>
2022-05-24 12:23:50 -0400

    changed relative imports to absolute imports

--- client/src/components/atomize_wrapper/Button.tsx
+++ client/src/components/atomize_wrapper/Button.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { colors } from "../../constants/colors";
+import { colors } from "@/constants/colors";
 import { Button } from "atomize";
 
 const _Button = (props: any) => <Button p="1rem" rounded="xl" bg={colors["fg"]} {...props} />; // eslint-disable-line

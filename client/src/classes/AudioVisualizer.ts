1fad39f153f88879f43c5fc9dbd8db5324ce4fb6
siberowl <kon.aoki.us@gmail.com>
2022-05-25 15:31:36 -0700

    bg-242 fixed bug by checking if active stream is an audiotrack

--- client/src/classes/AudioVisualizer.ts
+++ client/src/classes/AudioVisualizer.ts
@@ -11,37 +11,36 @@ export class AudioVisualizer {
   }
 
   setStream(stream: MediaStream): void {
-    if (stream) {
-      if (stream.active) {
-        const context = new AudioContext();
-        const source = context.createMediaStreamSource(stream);
-        const analyser = context.createAnalyser();
-        analyser.smoothingTimeConstant = 0.3;
-        analyser.fftSize = 1024;
-
-        // chain mic -> analyser -> processor -> context
-        source.connect(analyser); // feed mic audio into analyser
-
-        if (this.intervalId) {
-          window.clearInterval(this.intervalId);
-          this.onAudioActivity(0);
-        }
-        const draw = () => {
-          const array = new Uint8Array(analyser.fftSize);
-          analyser.getByteFrequencyData(array);
-
-          let sum = 0;
-          array.forEach((e) => {
-            sum += e * 4;
-          });
-          const average = sum / array.length;
-          if (average != 0) {
-            this.onAudioActivity(average);
-          }
-        };
-        this.intervalId = window.setInterval(draw, 50);
-      }
+    if (!stream || !stream.active || !stream.getAudioTracks().length) {
+      return;
     }
+    const context = new AudioContext();
+    const source = context.createMediaStreamSource(stream);
+    const analyser = context.createAnalyser();
+    analyser.smoothingTimeConstant = 0.3;
+    analyser.fftSize = 1024;
+
+    // chain mic -> analyser -> processor -> context
+    source.connect(analyser); // feed mic audio into analyser
+
+    if (this.intervalId) {
+      window.clearInterval(this.intervalId);
+      this.onAudioActivity(0);
+    }
+    const draw = () => {
+      const array = new Uint8Array(analyser.fftSize);
+      analyser.getByteFrequencyData(array);
+
+      let sum = 0;
+      array.forEach((e) => {
+        sum += e * 4;
+      });
+      const average = sum / array.length;
+      if (average != 0) {
+        this.onAudioActivity(average);
+      }
+    };
+    this.intervalId = window.setInterval(draw, 50);
   }
 }
 export const gainToMultiplier = (gain: number): number => {
@@ -54,4 +53,3 @@ export const gainToMultiplier = (gain: number): number => {
   }
   return _multiplier;
 };
-

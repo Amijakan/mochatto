54a9bfbd011a783d07d7ae37b65ad639d7c65663
Njima1572 <kochi.nakajima@gmail.com>
2022-05-27 06:46:59 -0400

    rf-258 usage of map

--- client/src/components/DeviceSelectorHelper.ts
+++ client/src/components/DeviceSelectorHelper.ts
@@ -12,33 +12,25 @@ const defaultOnError = (e: MediaStreamError): void => {
 // return a list of available input audio devices
 export const listInputDevices = (
   onError: (MediaStreamError) => void = defaultOnError
-): Promise<{ value: string; label: string }[]> => {
-  const inputs: { value: string; label: string }[] = [];
-  return navigator.mediaDevices
+): Promise<{ value: string; label: string }[]> => (
+  navigator.mediaDevices
     .enumerateDevices()
-    .then((devices) => {
-      devices.map((device) => {
-        if (device.kind === "audioinput") {
-          const input = Device();
-          input.value = device.deviceId;
-          input.label = device.label;
-          inputs.push(input);
-          return null;
-        }
-      });
-      return inputs;
-    })
+    .then((devices) => devices.map((device) => {
+      if (device.kind === "audioinput") {
+        return { label: device.label, value: device.deviceId }
+      }
+    }))
     .catch((e) => {
       onError(e);
       return [Device()];
-    });
-};
+    })
+);
 
 // select a device as the input device
 export const selectInputDevice = (
   id: string,
   useStream: (MediaStream) => void,
-  options: {autoGainControl: boolean, echoCancellation: boolean, noiseSuppression: boolean},
+  options: { autoGainControl: boolean, echoCancellation: boolean, noiseSuppression: boolean },
   onError: (MediaStreamError) => void = defaultOnError
 ): void => {
   navigator.mediaDevices

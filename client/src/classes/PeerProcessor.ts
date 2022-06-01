468f53e0fb28854b5b44448d9adf86cfc102a6c9
Kon Aoki <kon.aoki.us@gmail.com>
2022-05-31 21:38:16 -0400

    ft-209 fixed variable

--- client/src/classes/PeerProcessor.ts
+++ client/src/classes/PeerProcessor.ts
@@ -250,7 +250,7 @@ export class PeerProcessor {
         // If video player doesn't exist, create.
         this.videoPlayer ??= document.createElement("video");
         // Append player to div.
-        document.getElementById("avatar-video-" + this.peerId)?.appendChild(videoPlayer);
+        document.getElementById("avatar-video-" + this.peerId)?.appendChild(this.videoPlayer);
         // Set the new stream as the video source and play.
         this.videoPlayer.srcObject = this.peerStream;
         this.videoPlayer.play();

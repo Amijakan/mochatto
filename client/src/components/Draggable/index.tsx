a49a95f6efb8cbf02cea122f1c274a2e60d5ee3f
Njima1572 <kochi.nakajima@gmail.com>
2022-06-04 18:51:51 -0400

    ft-292 Added draggable component

---
+++ client/src/components/Draggable/index.tsx
@@ -0,0 +1,50 @@
+import React, { useRef, useCallback } from 'react'
+
+const Draggable = (
+  {
+    children,
+    position,
+    updatePosition = () => { },
+    draggable = true,
+  }: {
+    children: React.FC | React.FC[]
+    position: { x: number, y: number },
+    updatePosition?: (position: { x: number, y: number }) => void,
+    draggable: boolean
+  }
+) => {
+  // Offset for the difference between center of element and where the click actually occurred
+  const offsetRef = useRef({ x: 0, y: 0 })
+  const positionRef = useRef(position)
+
+  const onPointerDown = useCallback((event) => {
+    offsetRef.current = { x: event.clientX - positionRef.current.x, y: event.clientY - positionRef.current.y };
+    document.addEventListener("pointermove", _onPointerMove, true);
+    document.addEventListener("pointerup", _onPointerUp, true);
+    event.preventDefault();
+  }, []);
+
+  // remove listeners on mouse up
+  const _onPointerUp = useCallback(() => {
+    document.removeEventListener("pointermove", _onPointerMove, true);
+    document.removeEventListener("pointerup", _onPointerUp, true);
+  }, []);
+
+  // update mouse position on move
+  const _onPointerMove = useCallback((event) => {
+    // world coordinate
+    const newPosition = { x: event.clientX - offsetRef.current.x, y: event.clientY - offsetRef.current.y };
+    positionRef.current = newPosition
+    updatePosition(newPosition)
+  }, []);
+
+  return (
+    <div
+      {...(draggable ? { onPointerDown: onPointerDown } : {})}
+      style={{ position: 'absolute', top: position.y, left: position.x }}
+    >
+      {children}
+    </div>)
+}
+
+export default Draggable

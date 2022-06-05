9dab28620c834b6568a9fb5c6297e88831c345da
Amijakan <kochi.nakajima@gmail.com>
2022-06-05 16:25:35 -0400

    Ft-283 Added simple visual test (#300)
    * Added a simple test
    
    * Updated playwright config
    
    * Created a test with GUI
    
    * Optimized docker commands
    
    * Updated README.md
    
    * Little cleanups
    
    * Removed target from docker-compose

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -1,6 +1,13 @@
 import { PlaywrightTestConfig } from '@playwright/test'
 
+// Reference: https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
 const config: PlaywrightTestConfig = {
+  webServer: {
+    command: 'yarn dev',
+    port: 4500,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
   reporter: process.env.CI ? 'github' : 'list'
 };
 export default config

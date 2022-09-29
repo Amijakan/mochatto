61ebe41d46757ab810a3f48c300e48bd5bd79e87
Kon Aoki <kon.aoki.us@gmail.com>
2022-09-28 23:12:00 -0400

    ft-74 added authentication to backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,12 @@ import express from "express";
 import { Server } from "socket.io";
 import cors from "cors";
 import fs from "fs";
-
-const stripTrailingSlash = (str: string) => str.replace(/\/$/, '')
+import { createHash } from "crypto";
+import path from "path";
 
 const app = express();
 const port = 4000;
-const isProd = process.env.MODE === "prod"
+const isProd = process.env.MODE === "prod";
 const server = app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
 });
@@ -18,16 +18,47 @@ const io = new Server(server, {
   },
 });
 
+enum AuthenticationEnum {
+  Success = 0,
+  IncorrectPassword = 401,
+}
+
+const rooms: { [key: string]: { numUsers: number; passHash: string } } = {};
+
+// Function to decide whether to verify room password.
+const authenticate = (roomName: string, pass: string) => {
+  const hash = createHash("sha256")
+      .update(roomName + pass)
+      .digest("hex"),
+    room = rooms[roomName];
+
+  // If room doesn't exist, return.
+  if (!room) {
+    rooms[roomName] = { numUsers: 0, passHash: hash };
+    return AuthenticationEnum.Success;
+  } else {
+    const roomHash = room.passHash;
+    // If room password is set, authenticate, and if not, set password and allow connection.
+    if (roomHash) {
+      if (roomHash == hash) return AuthenticationEnum.Success;
+      else return AuthenticationEnum.IncorrectPassword;
+    }
+  }
+};
+
+const getRoomName = (nsp: string) => {
+  return encodeURIComponent(path.basename(nsp));
+};
+
 app.get("/", (req, res) => {
-  res.send({ body: "Hello world, 3" });
+  res.send({ body: "Hello world, 4" });
 });
 
-const rooms: { [key: string]: number } = {};
-
-const users: object[] = [];
 io.on("connection", (socket) => {
+  console.log("made connection");
   socket.on("NUM_USERS", (nspName) => {
-    const roomName = stripTrailingSlash(nspName)
+    console.log(nspName);
+    const roomName = getRoomName(nspName);
     socket.emit("NUM_USERS", rooms[roomName]);
   });
 });
@@ -36,51 +67,66 @@ io.of((nsp, query, next) => {
   const { token } = query;
   // authentication
 
+  console.log("test");
   // If success
   next(null, true);
 }).on("connection", (socket) => {
   console.log("new client: " + socket.id);
-  const roomName = stripTrailingSlash(socket.nsp.name)
-  socket.on("JOIN", (name) => {
-    const user = { name, id: socket.id };
-    if (name !== "") {
-      if (rooms[roomName]) {
-        rooms[roomName] += 1;
-      } else {
-        rooms[roomName] = 1;
-      }
-      console.log(name + " (" + socket.id + ") has joined namespace " + roomName);
-      io.of(roomName).emit("JOIN", user);
-    }
-  });
+  const roomName = getRoomName(socket.nsp.name);
 
-  socket.on("OFFER", (dataString) => {
-    const targetId = JSON.parse(dataString).receiverId;
-    console.log(socket.id + " has sent offer to " + targetId);
-    io.of(roomName).to(targetId).emit("OFFER", dataString);
-  });
-  socket.on("ANSWER", (dataString) => {
-    const targetId = JSON.parse(dataString).receiverId;
-    console.log(socket.id + " has sent answer to " + targetId);
-    io.of(roomName).to(targetId).emit("ANSWER", dataString);
-  });
-  socket.on("ICE_CANDIDATE", (dataString) => {
-    const targetId = JSON.parse(dataString).receiverId;
-    io.of(roomName).to(targetId).emit("ICE_CANDIDATE", dataString);
-  });
-  socket.on("SDP_RECEIVED", (sdpSenderId) => {
-    io.of(roomName).to(sdpSenderId).emit("SDP_RECEIVED");
-  });
-  socket.on("LEAVE", () => {
-    console.log("user's leaving");
-    io.of(roomName).emit("LEAVE", { id: socket.id });
-  });
-  socket.on("disconnect", () => {
-    rooms[roomName] -= 1;
-    if (rooms[roomName] === 0) {
-      delete rooms[roomName];
+  socket.on("AUTHENTICATE", (password) => {
+    const authenticationResult = authenticate(roomName, password);
+    console.log("authenticate: " + authenticationResult);
+    socket.emit("AUTHENTICATE", authenticationResult);
+
+    if (authenticationResult == AuthenticationEnum.Success) {
+      socket.on("JOIN", (name) => {
+        const user = { name, id: socket.id };
+        if (name !== "") {
+          if (rooms[roomName].numUsers) {
+            rooms[roomName].numUsers += 1;
+          } else {
+            rooms[roomName].numUsers = 1;
+          }
+          console.log(name + " (" + socket.id + ") has joined namespace " + roomName);
+          io.of(roomName).emit("JOIN", user);
+        }
+      });
+
+      socket.on("OFFER", (dataString) => {
+        const targetId = JSON.parse(dataString).receiverId;
+        console.log(socket.id + " has sent offer to " + targetId);
+        io.of(roomName).to(targetId).emit("OFFER", dataString);
+      });
+
+      socket.on("ANSWER", (dataString) => {
+        const targetId = JSON.parse(dataString).receiverId;
+        console.log(socket.id + " has sent answer to " + targetId);
+        io.of(roomName).to(targetId).emit("ANSWER", dataString);
+      });
+
+      socket.on("ICE_CANDIDATE", (dataString) => {
+        const targetId = JSON.parse(dataString).receiverId;
+        io.of(roomName).to(targetId).emit("ICE_CANDIDATE", dataString);
+      });
+
+      socket.on("SDP_RECEIVED", (sdpSenderId) => {
+        io.of(roomName).to(sdpSenderId).emit("SDP_RECEIVED");
+      });
+
+      socket.on("LEAVE", () => {
+        console.log("user's leaving");
+        io.of(roomName).emit("LEAVE", { id: socket.id });
+      });
+
+      socket.on("disconnect", () => {
+        rooms[roomName].numUsers -= 1;
+        if (rooms[roomName].numUsers === 0) {
+          delete rooms[roomName];
+        }
+        io.of(roomName).emit("DISCONNECT", { id: socket.id });
+        console.log(socket.id + " has disconnected.");
+      });
     }
-    io.of(roomName).emit("DISCONNECT", { id: socket.id });
-    console.log(socket.id + " has disconnected.");
   });
 });

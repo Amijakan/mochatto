a1f59887b09afe17574ab27b148fe70e159b2276
siberowl <kon.aoki.us@gmail.com>
2023-05-20 21:51:55 -0400

    rf-359 removed stream from device context

--- client/src/contexts/DeviceContext.tsx
+++ client/src/contexts/DeviceContext.tsx
@@ -5,15 +5,14 @@ import { Device } from "@/components/DeviceSelector";
 export const DeviceContext = createContext<any>({});
 
 export const DeviceProvider = ({ children }: { children: JSX.Element }): JSX.Element => {
-  const [stream, setStream] = useState(new MediaStream());
   const [inputOptions, setInputOptions] = useState([{}]);
   const [selectedInput, setSelectedInput] = useState(Device());
   const [options, setOptions] = useState({autoGainControl: true, echoCancellation: true, noiseSuppression: false});
 
   return (
     <DeviceContext.Provider
-      value={{ stream, setStream, inputOptions, setInputOptions, selectedInput, setSelectedInput, options, setOptions }}
-    >
+      value={{ inputOptions, setInputOptions, selectedInput, setSelectedInput, options, setOptions }}
+    > 
       {children}
     </DeviceContext.Provider>
   );
